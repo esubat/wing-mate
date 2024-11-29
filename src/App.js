e5f93d685a0b24fc031d78dd468a0c961cff7adb
esuba <esuba419@gmail.com>
2024-11-29 13:11:47 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import SearchField from './components/SearchField';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <SearchField />
+    </>
+  );
+}
+
+export default App;
